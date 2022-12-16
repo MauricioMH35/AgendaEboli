5a35a085b4de571a8dc1f110f3f75ede1b59960d
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-12-16 16:07:20 -0300

    feat: contact parse between entity, response and request

--- src/main/java/br/com/eboli/models/requests/ContactRequest.java
+++ src/main/java/br/com/eboli/models/requests/ContactRequest.java
@@ -3,6 +3,7 @@ package br.com.eboli.models.requests;
 import br.com.eboli.models.Contact;
 import br.com.eboli.models.Customer;
 import br.com.eboli.models.enums.ContactType;
+import br.com.eboli.models.responses.ContactResponse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -31,6 +32,57 @@ public class ContactRequest {
                         .foundation(null)
                         .registered(null)
                         .build())
+                .build();
+    }
+
+    public ContactResponse parseToResponse() {
+        return ContactResponse.builder()
+                .id(this.id)
+                .type(this.type)
+                .contact(this.contact)
+                .customerId(this.customerId)
+                .build();
+    }
+
+    public static Contact parseToModel(ContactRequest request) {
+        return Contact.builder()
+                .id(request.id)
+                .type(request.type)
+                .contact(request.contact)
+                .customer(Customer.builder()
+                        .id(request.customerId)
+                        .fullname(null)
+                        .cnpj(null)
+                        .foundation(null)
+                        .registered(null)
+                        .build())
+                .build();
+    }
+
+    public static ContactResponse parseToResponse(ContactRequest request) {
+        return ContactResponse.builder()
+                .id(request.id)
+                .type(request.type)
+                .contact(request.contact)
+                .customerId(request.customerId)
+                .build();
+    }
+
+    public static ContactRequest parse(Contact model) {
+        return ContactRequest.builder()
+                .id(model.getId())
+                .type(model.getType())
+                .contact(model.getContact())
+                .customerId(model.getCustomer().getId())
+                .build();
+    }
+
+    public static ContactRequest parse(ContactResponse response) {
+        return ContactRequest.builder()
+                .id(response.getId())
+                .type(response.getType())
+                .contact(response.getContact())
+                .customerId(response.getCustomerId())
                 .build();
     }
 
