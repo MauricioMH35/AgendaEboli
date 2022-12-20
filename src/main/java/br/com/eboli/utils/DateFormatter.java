ff76f38750677698a74df2441f70c0979e136d42
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-12-20 16:37:50 -0300

    feat: check date formaters

--- src/main/java/br/com/eboli/utils/DateFormatter.java
+++ src/main/java/br/com/eboli/utils/DateFormatter.java
@@ -3,6 +3,8 @@ package br.com.eboli.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DateFormatter {
 
@@ -14,6 +16,20 @@ public class DateFormatter {
     public static String parseDateTime(LocalDateTime dateTime) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy.MM.dd HH-mm-ss");
         return dateTime.format(fmt);
+    }
+
+    public static  Boolean checkDatePattern(String target) {
+        final String regex = "(\\d{4}.\\d{2}.\\d{2})";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
+    public static Boolean checkDateTimePattern(String target) {
+        final String regex = "(\\d{4}.\\d{2}.\\d{2})_(\\d{2}-\\d{2}-\\d{2})";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(target);
+        return matcher.find();
     }
 
     public static LocalDate parseDate(String date) {
