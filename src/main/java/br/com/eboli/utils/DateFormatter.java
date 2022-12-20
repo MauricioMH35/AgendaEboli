1a5d3bf8c10446c5493dc0bed5b6574cb96400d3
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-12-20 17:12:19 -0300

    fix: check date formatter

--- src/main/java/br/com/eboli/utils/DateFormatter.java
+++ src/main/java/br/com/eboli/utils/DateFormatter.java
@@ -18,20 +18,6 @@ public class DateFormatter {
         return dateTime.format(fmt);
     }
 
-    public static  Boolean checkDatePattern(String target) {
-        final String regex = "(\\d{4}.\\d{2}.\\d{2})";
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(target);
-        return matcher.find();
-    }
-
-    public static Boolean checkDateTimePattern(String target) {
-        final String regex = "(\\d{4}.\\d{2}.\\d{2})_(\\d{2}-\\d{2}-\\d{2})";
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(target);
-        return matcher.find();
-    }
-
     public static LocalDate parseDate(String date) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy.MM.dd");
         return LocalDate.parse(date, fmt);
@@ -40,6 +26,21 @@ public class DateFormatter {
     public static String parseDate(LocalDate date) {
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy.MM.dd");
         return date.format(fmt);
+    }
+
+    public static  Boolean checkDatePattern(String target) {
+        final String regex = "(\\d{4})[^-\\/\\\\\\s](\\d{2})[^-\\/\\\\\\s](\\d{2})";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(target);
+        Boolean isValid = matcher.find();
+        return isValid;
+    }
+
+    public static Boolean checkDateTimePattern(String target) {
+        final String regex = "(\\d{4})[^-\\/\\\\\\s](\\d{2})[^-\\/\\\\\\s](\\d{2})[^-\\/\\\\\\s](\\d{2})[^-\\/\\\\\\s](\\d{2})[^-\\/\\\\\\s](\\d{2})";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(target);
+        return matcher.find();
     }
 
 }
