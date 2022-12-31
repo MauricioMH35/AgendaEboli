5d504414ad87ac7e40b6c2eeff4fb62698b1c95c
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-12-31 00:03:19 -0300

    feat: check if a string is a valid boolean value

--- src/main/java/br/com/eboli/utils/StringUtil.java
+++ src/main/java/br/com/eboli/utils/StringUtil.java
@@ -1,5 +1,8 @@
 package br.com.eboli.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringUtil {
 
     public static String replaceToSpace(String param) {
@@ -8,6 +11,14 @@ public class StringUtil {
 
     public static String replaceToUnderscore(String param) {
         return param.replaceAll(" ", "_");
+    }
+
+    public static Boolean isBooleanValid(String param) {
+        final String regex = "(true|false)";
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(param);
+        boolean valid = matcher.find();
+        return valid;
     }
 
 }
